31daa396e398665c7e4f0875301042a269344dcc
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2024-02-09 19:55:31 -0300

    Criando e renderizando o acesso padrão para a view index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/", function(req, res){
-    res.send("ola");
+    res.render("index");
 });
 
 app.listen(4000, ()=> {
