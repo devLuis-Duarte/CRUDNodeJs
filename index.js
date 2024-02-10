b283cb6cd8c078b643aa81dd7fc250e2c490181e
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2024-02-09 21:13:00 -0300

    Criando conexao com banco de dados mysql  no projeto

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express(); 
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+connection.authenticate()
+.then(()=>{
+console.log("banco conectado com sucesso");
+}).catch((error) => {
+    console.log(error);
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", function(req, res){
     res.render("index");
